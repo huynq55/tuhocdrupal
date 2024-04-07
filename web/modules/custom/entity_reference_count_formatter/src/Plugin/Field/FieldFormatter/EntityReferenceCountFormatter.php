b1d0338973ac34537465e6ab71fbba2e70685e05
Quang Huy <huynq23091@gmail.com>
2024-04-07 17:29:23 +0700

    Update EntityReferenceCountFormatter.php

--- web/modules/custom/entity_reference_count_formatter/src/Plugin/Field/FieldFormatter/EntityReferenceCountFormatter.php
+++ web/modules/custom/entity_reference_count_formatter/src/Plugin/Field/FieldFormatter/EntityReferenceCountFormatter.php
@@ -2,6 +2,8 @@
 
 namespace Drupal\entity_reference_count_formatter\Plugin\Field\FieldFormatter;
 
+use Drupal\Core\Cache\Cache;
+use Drupal\Core\Cache\CacheBackendInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
 use Drupal\Core\Field\FieldItemListInterface;
@@ -22,7 +24,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  */
 class EntityReferenceCountFormatter extends FormatterBase {
 
-  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, protected EntityTypeManagerInterface $entityTypeManager) {
+  public function __construct($plugin_id, $plugin_definition, FieldDefinitionInterface $field_definition, array $settings, $label, $view_mode, array $third_party_settings, protected EntityTypeManagerInterface $entityTypeManager, protected CacheBackendInterface $cacheBackend) {
     parent::__construct($plugin_id, $plugin_definition, $field_definition, $settings, $label, $view_mode, $third_party_settings);
   }
 
@@ -30,7 +32,7 @@ class EntityReferenceCountFormatter extends FormatterBase {
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($plugin_id, $plugin_definition, $configuration['field_definition'], $configuration['settings'], $configuration['label'], $configuration['view_mode'], $configuration['third_party_settings'], $container->get('entity_type.manager'));
+    return new static($plugin_id, $plugin_definition, $configuration['field_definition'], $configuration['settings'], $configuration['label'], $configuration['view_mode'], $configuration['third_party_settings'], $container->get('entity_type.manager'), $container->get('cache.default'));
   }
 
   /**
@@ -42,21 +44,28 @@ class EntityReferenceCountFormatter extends FormatterBase {
 
     foreach ($items as $delta => $item) {
       $referencedEntity = $item->entity;
-
       $fieldDefinition = $item->getFieldDefinition();
       $entityType = $fieldDefinition->getTargetEntityTypeId();
       $entityBundle = $fieldDefinition->getTargetBundle();
       $referenceField = $fieldDefinition->getName();
+      $cacheId = "entity_reference_count:{$entityType}:{$entityBundle}:{$referenceField}:{$referencedEntity->id()}";
 
-      $query = $this->entityTypeManager->getStorage($entityType)->getQuery()
-        ->accessCheck(FALSE)
-        ->condition($referenceField, $referencedEntity->id());
-
-      if ($filterByBundle) {
-        $query->condition('type', $entityBundle);
+      $cache = $this->cacheBackend->get($cacheId);
+      if ($cache) {
+        $referenceCount = $cache->data;
       }
+      else {
+        $query = $this->entityTypeManager->getStorage($entityType)->getQuery()
+          ->accessCheck(FALSE)
+          ->condition($referenceField, $referencedEntity->id());
 
-      $referenceCount = $query->count()->execute();
+        if ($filterByBundle) {
+          $query->condition('type', $entityBundle);
+        }
+
+        $referenceCount = $query->count()->execute();
+        $this->cacheBackend->set($cacheId, $referenceCount, Cache::PERMANENT, ['entity_reference_count:' . $entityType]);
+      }
 
       $entityUrl = $referencedEntity->toUrl()->toString();
 
