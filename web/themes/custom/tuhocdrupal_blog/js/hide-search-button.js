a154109815f6661d805e6d8313e52f230a659c7b
Quang Huy <huynq23091@gmail.com>
2024-04-10 00:07:28 +0700

    Update hide-search-button.js

--- web/themes/custom/tuhocdrupal_blog/js/hide-search-button.js
+++ web/themes/custom/tuhocdrupal_blog/js/hide-search-button.js
@@ -1,23 +1,34 @@
 (function ($, Drupal) {
-  Drupal.behaviors.autoSearchBasedOnClass = {
+  Drupal.behaviors.persistentHideSearchButton = {
     attach: function (context, settings) {
-      // Lấy ra nút tìm kiếm và ô nhập văn bản dựa vào class của view
-      var $searchButton = $('.view-search-content #edit-submit-search-content', context);
-      var $textInput = $('.view-search-content #edit-search-api-fulltext', context);
+      // Hàm để ẩn nút tìm kiếm
+      function hideSearchButton() {
+        var $searchButton = $('.view-search-content #edit-submit-search-content');
+        if (!$searchButton.hasClass('d-none')) {
+          $searchButton.addClass('d-none');
+        }
+      }
 
-      // Ẩn nút tìm kiếm
-      $searchButton.addClass('d-none');
+      // Ẩn nút tìm kiếm lần đầu tiên khi behavior được áp dụng
+      hideSearchButton();
+
+      // Lắng nghe sự kiện AJAX hoàn tất và ẩn nút tìm kiếm mỗi lần như vậy
+      $(document).ajaxComplete(function () {
+        hideSearchButton();
+      });
+
+      var $textInput = $('.view-search-content #edit-search-api-fulltext', context);
 
       // Tự động bấm nút tìm kiếm khi trang được tải
       $(document).ready(function () {
-        $searchButton.click();
+        $('.view-search-content #edit-submit-search-content').click();
       });
 
       // Tự động tìm kiếm khi người dùng nhập ít nhất 3 ký tự
       $textInput.on('keyup', function () {
         var searchValue = $(this).val();
         if (searchValue.length >= 3) {
-          $searchButton.click();
+          $('.view-search-content #edit-submit-search-content').click();
         }
       });
     }
