a157d62c4ab88b1badedbd6ac7b869dfb8712d01
Quang Huy <huynq23091@gmail.com>
2024-04-10 00:00:20 +0700

    update

---
+++ web/themes/custom/tuhocdrupal_blog/js/hide-search-button.js
@@ -0,0 +1,25 @@
+(function ($, Drupal) {
+  Drupal.behaviors.autoSearchBasedOnClass = {
+    attach: function (context, settings) {
+      // Lấy ra nút tìm kiếm và ô nhập văn bản dựa vào class của view
+      var $searchButton = $('.view-search-content #edit-submit-search-content', context);
+      var $textInput = $('.view-search-content #edit-search-api-fulltext', context);
+
+      // Ẩn nút tìm kiếm
+      $searchButton.hide();
+
+      // Tự động bấm nút tìm kiếm khi trang được tải
+      $(document).ready(function () {
+        $searchButton.click();
+      });
+
+      // Tự động tìm kiếm khi người dùng nhập ít nhất 3 ký tự
+      $textInput.on('keyup', function () {
+        var searchValue = $(this).val();
+        if (searchValue.length >= 3) {
+          $searchButton.click();
+        }
+      });
+    }
+  };
+})(jQuery, Drupal);
