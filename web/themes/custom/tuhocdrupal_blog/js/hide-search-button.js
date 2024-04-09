46d89edd200d01569968cd377186c3fd35c01d4b
Quang Huy <huynq23091@gmail.com>
2024-04-10 00:18:37 +0700

    Update hide-search-button.js

--- web/themes/custom/tuhocdrupal_blog/js/hide-search-button.js
+++ web/themes/custom/tuhocdrupal_blog/js/hide-search-button.js
@@ -1,11 +1,15 @@
 (function ($, Drupal) {
   Drupal.behaviors.persistentHideSearchButton = {
     attach: function (context, settings) {
+      var searchButtonHidden = false; // Cờ để kiểm tra xem nút tìm kiếm đã được ẩn hay không
+      var initialSearchButtonClicked = false; // Cờ để kiểm tra xem đã click vào nút tìm kiếm khi trang được tải hay không
+
       // Hàm để ẩn nút tìm kiếm
       function hideSearchButton() {
         var $searchButton = $('.view-search-content [id^="edit-submit-search-content"]');
         if (!$searchButton.hasClass('d-none')) {
           $searchButton.addClass('d-none');
+          searchButtonHidden = true; // Đặt cờ là true khi nút tìm kiếm đã được ẩn
         }
       }
 
@@ -14,15 +18,20 @@
 
       // Lắng nghe sự kiện AJAX hoàn tất và ẩn nút tìm kiếm mỗi lần như vậy
       $(document).ajaxComplete(function () {
-        hideSearchButton();
+        if (!searchButtonHidden) {
+          hideSearchButton(); // Ẩn nút tìm kiếm chỉ khi nó chưa được ẩn
+        }
       });
 
       var $textInput = $('.view-search-content #edit-search-api-fulltext', context);
 
       // Tự động bấm nút tìm kiếm khi trang được tải
-      $(document).ready(function () {
-        $('.view-search-content [id^="edit-submit-search-content"]').click();
-      });
+      if (!initialSearchButtonClicked) {
+        $(document).ready(function () {
+          $('.view-search-content [id^="edit-submit-search-content"]').click();
+          initialSearchButtonClicked = true; // Đặt cờ là true sau khi click vào nút tìm kiếm khi trang được tải
+        });
+      }
 
       // Tự động tìm kiếm khi người dùng nhập ít nhất 3 ký tự
       $textInput.on('keyup', function () {
