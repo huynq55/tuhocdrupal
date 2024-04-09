d3042240a1e8558f4635631628c77d7e4840fa4a
Quang Huy <huynq23091@gmail.com>
2024-04-10 00:37:20 +0700

    Update hide-search-button.js

--- web/themes/custom/tuhocdrupal_blog/js/hide-search-button.js
+++ web/themes/custom/tuhocdrupal_blog/js/hide-search-button.js
@@ -1,7 +1,7 @@
 (function ($, Drupal) {
   Drupal.behaviors.simpleSearchBehavior = {
     attach: function (context, settings) {
-      var $textInput = $('.view-search-content #edit-search-api-fulltext', context);
+      var $textInput = $('.view-search-content [id^="edit-search-api-fulltext"]', context);
 
       // Tự động tìm kiếm khi người dùng nhập ít nhất 3 ký tự
       $textInput.on('keyup', function () {
